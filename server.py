77b33842f453a00788c2cf287d296ca2320fadac
Pratik Bhandarkar <pratik.bhandarkar@sjsu.edu>
2018-09-09 20:34:14 -0700

    added informative messages in server code

--- server.py
+++ server.py
@@ -15,9 +15,11 @@ class Server(calculator_pb2_grpc.AdderServicer):
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10));
 calculator_pb2_grpc.add_AdderServicer_to_server(Server(), server);
 server.add_insecure_port('[::]:15432');
+print("Started server on port '[::]:15432'...");
 server.start();
 try:
 	while True:
+		print("Waiting for request...");
 		time.sleep(60 * 60);
 except KeyboardInterrupt:
 	server.stop(0);
